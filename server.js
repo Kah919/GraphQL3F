7fd15f53ee54d2e153640f98073582f9d5466f94
Kah919 <kahyap13@gmail.com>
2020-10-09 20:02:47 -0400

    Authors can now query books

--- server.js
+++ server.js
@@ -41,7 +41,11 @@ const AuthorType = new GraphQLObjectType({
     description: 'This represents an author',
     fields: () => ({
         id: { type: GraphQLNonNull(GraphQLInt) },
-        name: { type: GraphQLNonNull(GraphQLString) }
+        name: { type: GraphQLNonNull(GraphQLString) },
+        books: { 
+            type: new GraphQLList(BookType),
+            resolve: (author) => books.filter(book => book.authorId === author.id) 
+        }
     })
 })
 
